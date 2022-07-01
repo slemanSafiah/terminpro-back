b99987b496a0eb9d1fd02215931178c5c150d513
Sleman Safiah <148073@appareluae.com>
2022-07-01 16:42:15 +0400

    try change cors

--- src/server.js
+++ src/server.js
@@ -24,9 +24,18 @@ app.use(bodyParser.json({ limit: '50mb' }));
 
 //************************** Access Origin ****************************************/
 app.use((req, res, next) => {
-	res.header('Access-Control-Allow-Origin', '*');
-	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-	res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
+	// res.header('Access-Control-Allow-Origin', '*');
+	// res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+	// res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
+	res.setHeader('Access-Control-Allow-Credentials', true);
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	// another common pattern
+	// res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+	res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+	res.setHeader(
+		'Access-Control-Allow-Headers',
+		'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+	);
 	next();
 });
 
